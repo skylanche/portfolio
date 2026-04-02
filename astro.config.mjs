511dac3b201b66937452af55ff3cf7c6dea7b00b
Remone Teleporta <remoneteleporta@gmail.com>
2026-04-02 17:07:12 +0530

    remove trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sakthivk-portfolio.vercel.app/'
+    site: 'https://sakthivk-portfolio.vercel.app/',
+    trailingSlash: 'never',
 });
